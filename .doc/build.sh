109d796001b217243f5e00ed9acbdf201c2188ca
guodahai <773244804>
2018-10-25 15:43:29 +0800

    脚本

---
+++ .doc/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PROJECT_HOME=spring-boot-quartz
+TARGET_DIR=/ocean/docker/$PROJECT_HOME/
+if [ ! -d $TARGET_DIR  ];then
+  mkdir $TARGET_DIR
+else
+  echo $TARGET_DIR exist
+fi
+BIN_DIR=`pwd`
+cp $BIN_DIR/quartz/target/*.jar $TARGET_DIR
+cp $BIN_DIR/quartz/target/Dockerfile $TARGET_DIR
+cd $TARGET_DIR
+docker build -t ocean/$PROJECT_HOME:1.0-SNAPSHOT .
+
